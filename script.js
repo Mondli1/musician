65186c0d5b2785c146d742e01f10ae111e85e1ba
Sam Smyth <smyth.j.sam@gmail.com>
2019-07-01 22:52:14 -0700

    Starting JS slider

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ window.addEventListener("scroll", function() {
     navBar.classList.remove("nav-shrink");
   }
 });
+
+const slide = document.querySelector('.slide');
+const slideImg = document.querySelectorAll('.slide img');
+let currentSlide = 0;
+const size = slide.clientWidth;
+
+function changeSlide(newSlide) {
+  slide.style.transform = 'translateX(' + (-size * newSlide) + 'px)';
+  slide.style.transition = ' .2s';
+}
+
+function autoSlide() {
+  if (currentSlide < 2) {
+    currentSlide++;
+    changeSlide(currentSlide);
+  }
+  if (currentSlide === 2) {
+    slide.style.transform = 'translateX(0)';
+    currentSlide = 0;
+  }
+}
+
+setInterval(autoSlide, 1000);
