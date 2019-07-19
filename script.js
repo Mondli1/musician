ba325dd29f75892417b039fe8cf026e279bc8f48
Sam-Smyth <smyth.j.sam@gmail.com>
2019-07-18 20:57:00 -0700

    Finished all responsive. Mobile menu. JS. DONE!

--- script.js
+++ script.js
@@ -30,3 +30,26 @@ function autoSlide() {
 }
 
 setInterval(autoSlide, 3000);
+
+const mobileMnuBtn = document.querySelector('.burger');
+const mobileMnu = document.querySelector('.mobile-menu');
+const overlay = document.querySelector('.tinted-overlay');
+
+mobileMnuBtn.addEventListener('click', function() {
+  mobileMnu.classList.toggle('mobile-menu-expand');
+  overlay.classList.toggle('tinted-overlay-show');
+});
+
+document.addEventListener('click', function() {
+  mobileMnu.classList.remove('mobile-menu-expand');
+  overlay.classList.remove('tinted-overlay-show');
+});
+
+mobileMnu.addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+
+mobileMnuBtn.addEventListener('click', function(event) {
+  event.stopPropagation();
+});
+
